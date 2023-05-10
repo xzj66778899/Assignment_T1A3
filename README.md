6b8ffe2cfd5836b6734e220b601b87ee445d57bd
xzj66778899 <xzj66778899@gmail.com>
2023-05-10 22:52:18 +1000

    new feature added - automatically update the carlist when purchase completed

---
+++ README.md
@@ -0,0 +1,21 @@
+Introduction:
+    This is a car sale terminal application that helps buyers to select a car in the stock then creat a purchase order.
+
+Git repo link: https://github.com/xzj66778899/Assignment_T1A3.gi
+
+Code style guide:  ‘Style Guide for Python Code’ （https://peps.python.org/pep-0008/）
+
+APP features: 
+    Feature 1: 
+        As a sales, you can also edit the carlist in the CSV file to let the buyers to choose. Now there're 50 cars in the stock.
+        Import the carlist in CSV format.
+        The result is to output a purchase order with information such as 'car details', 'loan details' and 'sales date' in TXT format, and the stocklist will autopmatically updated when an order is completed. The sold car details will bu put in car_sold list.
+
+    Feature 2:
+        As a buyer, you can choose any criteria within 'Type', 'Brand', 'Year', 'KM' and 'Price' to find the right car. 
+        When cars have been selected, you can choose either save it to built-in wishlist for later review, or to select again, or to the next step.
+
+    Feature 3:
+        After a car has been 'choosen' by the buyer, system will ask the user whether to consider a loan offer. If the buyer has interest, system will show a monthly payment according to the term chosen. Buyer can accept offer or reject the offer.
+    
+    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import pandas
 import loan_calculator
 
 Interest_rate = 1.07
@@ -21,7 +22,6 @@ def look_wishlist():
         for key, value in i.items():
             print(key,":",value,)
     return k
-
 
 # function to filter number in range(Year,KM and Price)
 def filter_number_range(word1,word2,criteria,list):
@@ -92,17 +92,14 @@ def filter_number_range(word1,word2,criteria,list):
             list.extend(i)
         break
 
-
 stocklist = [] 
 wishlist = []
 
 print("G'day! Welcome to our 'Save Your Money' car seller!\nFollow the steps with us you will find your dream car!")
-
 # filter the Brand
 flag = 1
 while flag:
     in_wishlist = 0
-
      # print the Brand in stocklist with no repetions
     while True:
         brand = []
@@ -144,44 +141,31 @@ while flag:
             # won't give user guide to enter 'look' if wishlist is empty
             if wishlist == []: 
                 # filter the Year
-                filter_number_range("\nPlease enter the min year of your car:\n\
-                                    ('quit' to exit or 'skip' to pass)\n",
-                                    "\nPlease enter the max year of your car:\n\
-                                    ('quit' to exit or 'skip' to pass)\n",
+                filter_number_range("\nPlease enter the min year of your car:\n('quit' to exit or 'skip' to pass)\n",
+                                    "\nPlease enter the max year of your car:\n('quit' to exit or 'skip' to pass)\n",
                                     'Year',stocklist)
                 # filter the KM
-                filter_number_range("\nPlease enter the min KM of your car:\n\
-                                    ('quit' to exit or 'skip' to pass)\n",
-                                    "\nPlease enter the max KM of your car:\n\
-                                    ('quit' to exit or 'skip' to pass)\n",
+                filter_number_range("\nPlease enter the min KM of your car:\n('quit' to exit or 'skip' to pass)\n",
+                                    "\nPlease enter the max KM of your car:\n('quit' to exit or 'skip' to pass)\n",
                                     'KM',stocklist)
                 # filter the Price
-                filter_number_range("\nPlease enter the min price of your car:\n\
-                                    ('quit' to exit or 'skip' to pass)\n",
-                                    "\nPlease enter the max price of your car:\n\
-                                    ('quit' to exit or 'skip' to pass)\n",
+                filter_number_range("\nPlease enter the min price of your car:\n('quit' to exit or 'skip' to pass)\n",
+                                    "\nPlease enter the max price of your car:\n('quit' to exit or 'skip' to pass)\n",
                                     'Price',stocklist)
                 
             if wishlist != []:
                 # filter the Year
-                filter_number_range("\nPlease enter the min year of your car:\n\
-                                    ('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
-                                    "\nPlease enter the max year of your car:\n\
-                                    ('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
+                filter_number_range("\nPlease enter the min year of your car:\n('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
+                                    "\nPlease enter the max year of your car:\n('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
                                     'Year',stocklist)
                 # filter the KM
-                filter_number_range("\nPlease enter the min KM of your car:\n\
-                                    ('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
-                                    "\nPlease enter the max KM of your car:\n\
-                                    ('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
+                filter_number_range("\nPlease enter the min KM of your car:\n('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
+                                    "\nPlease enter the max KM of your car:\n('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
                                     'KM',stocklist)
                 # filter the Price
-                filter_number_range("\nPlease enter the min price of your car:\n\
-                                    ('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
-                                    "\nPlease enter the max price of your car:\n\
-                                    ('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
+                filter_number_range("\nPlease enter the min price of your car:\n('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
+                                    "\nPlease enter the max price of your car:\n('quit' to exit or 'skip' to pass or 'look' to wishlist)\n",
                                     'Price',stocklist)
-
 
             if flag1:
                 print("\nWe have chosen the cars below for you!\n")
@@ -196,6 +180,7 @@ while flag:
                 n = len(wishlist)+1
             break
         break
+
     #  user decide to save, choose or select again.
     while True:
         if wishlist == []:
@@ -235,7 +220,6 @@ while flag:
             print("\nPlease enter valid answer!")
     continue
 
-
 if in_wishlist == False:   
     for i in range(n - 1):
         print(i + 1,' ' ,end='')
@@ -248,7 +232,6 @@ if in_wishlist == False:
         else:
             break
         
-    
 if in_wishlist == True:
     for i in range(m-1):
         print(i+1, ' ', end = '')
@@ -263,7 +246,6 @@ if in_wishlist == True:
 
 
 # print(car_chosen)
-
 print("\nThanks for choosing the car!\n")
 for k, v in car_chosen.items():
     print(k, ':', v)
@@ -274,7 +256,6 @@ if int(car_chosen['Discount']) != 0:
 
 if int(car_chosen['Discount']) == 0:
     Price=int(car_chosen['Price'])
-
 
 while True:
     offer_answer = input("\nWould you like to see our loan offer? 'Yes' or 'No'\n")
@@ -300,7 +281,7 @@ while flag3 == 1:
             print("Sorry to see you leave, you can run this program again to choose a car for you.")
             exit()
         try:
-            1 <= int(term) <= 5 is True
+            1 <= int(term) <= 5 == True
         except:
             print("Please enter a valid number between 1 to 5 ! ('quit' to exit)")
             continue
@@ -329,7 +310,6 @@ while flag3 == 1:
             continue
     flag3 = "order_with_loan"
 
-
 if flag3 == "order_with_loan":
     print("Here's your purchase order! Please find in TXT file!")
 # output purchase order to txt with loan information
@@ -338,18 +318,13 @@ if flag3 == "order_with_loan":
         for k,v in car_chosen.items():
             print(k, ':', v, file = f)
 
-        amount = loan_calculator.loan_summary(Price,Interest_rate,Term,Date)[1]
-        interest = loan_calculator.loan_summary(Price,Interest_rate,Term,Date)[3]
-        monthly_payment = round((loan_calculator.loan_summary(Price,Interest_rate,Term,Date)[1])/(12*Term),2)
+        amount = loan_calculator.loan_summary(Price, Interest_rate, Term,Date)[1]
+        interest = loan_calculator.loan_summary(Price, Interest_rate, Term,Date)[3]
+        monthly_payment = round((loan_calculator.loan_summary(Price, Interest_rate, Term, Date)[1])/(12*Term), 2)
 
-        print(f"\n\nloan amount:${amount}\nterm: {Term} years\n\
-              interest:${interest}\ninterest rate:{Interest_rate}\n\
-              monthly payment:${monthly_payment}\n\npurchase date:{Date}\n\
-              sales: online",file = f)
+        print(f"\n\nloan amount:${amount}\nterm: {Term} years\ninterest:${interest}\ninterest rate:{Interest_rate}\nmonthly payment:${monthly_payment}\n\npurchase date:{Date}\nsales: online", file = f)
         
-    print("\nThanks for chooing 'Save Your Money' car dealer! \
-        Have a nice day and looking forward to see you again!\n")
-
+    print("\nThanks for chooing 'Save Your Money' car dealer!\nHave a nice day and looking forward to see you again!\n")
 
 elif flag3 == 0:
     print("Here's your purchase order! Please find in TXT file!")
@@ -358,6 +333,21 @@ elif flag3 == 0:
         print('Pruchase Order\n\ncar details:',file=f)
         for k,v in car_chosen.items():
             print(k,':',v,file=f)
-        print(f"purchase date:{Date}\nsales:online",file=f)
-    print("\nThanks for chooing 'Save Your Money' car dealer! \
-          Have a nice day and looking forward to see you again!\n")
+        print(f"Price after discount: ${round(Price)}\n\npurchase date:\n{Date}\nsales:\nonline",file=f)
+    print("\nThanks for chooing 'Save Your Money' car dealer!\nHave a nice day and looking forward to see you again!\n")
+
+
+# update the car stocklist
+# use pandas get the car list
+csv_to_dirc()
+car_list = pandas.read_csv('carlist.csv')
+
+# find the sold car in carlist
+index=stocklist.index(car_chosen)
+car_sold = car_list.iloc[index:(index + 1)]
+
+# copy it to sold list
+car_sold.to_csv('car_sold.csv', mode = 'a', header = None, index = None)
+
+# remove the car from carlist
+car_list.drop([index]).to_csv('carlist.csv', index = None)
