01ce5340184f22efcb44c602eb7c379c6fde1cf5
xzj66778899 <xzj66778899@gmail.com>
2023-05-11 14:47:41 +1000

    add a bash script file

---
+++ my_wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if ! [[ -x "$(command -v python)" ]]
+then
+  echo "ERROR:
+    Python not installed. 
+    Enable to run the application, please refer to https://installpython3.com/ to install Python." >&2
+  exit 1
+fi
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r ./piplist.txt
+python3 main.py
