4d2d1522e9fab11e27d9a5aa5a205316ed99627e
xzj66778899 <xzj66778899@gmail.com>
2023-05-12 13:34:32 +1000

    edit bash script

--- src/my_wrapper.sh
+++ src/my_wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if ! [[ -x "$(command -v python)" ]]
+if ! [[ -x "$(command -v python3)" ]]
 then
   echo "ERROR:
-    Python not installed. 
+    Python3 not installed. 
     Enable to run the application, please refer to https://installpython3.com/ to install Python." >&2
   exit 1
 fi
 
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r ./requirements.txt
+python3 -m pip install -r ./requirements.txt
 python3 main.py
